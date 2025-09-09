f4852726f9548cafb0f611686b756cfec025cf7d
hndraphyy <hendraaditya1913@gmail.com>
2025-09-09 14:06:20 +0700

    feat: add experience about

--- src/components/sections/about/AboutMeSection.tsx
+++ src/components/sections/about/AboutMeSection.tsx
@@ -22,9 +22,10 @@ const AboutMeSection = () => {
               {aboutContent.skills.map((skill) => (
                 <li
                   key={skill.name}
-                  className="flex flex-col items-center gap-2 hover:scale-105 transition"
+                  className="flex flex-col items-center gap-2 hover:scale-[1.08] duration-500"
                 >
                   <Image
+                    title={skill.name}
                     src={skill.icon}
                     alt={skill.name}
                     width={40}
@@ -34,6 +35,37 @@ const AboutMeSection = () => {
                 </li>
               ))}
             </ul>
+          </div>
+
+          <div>
+            <div></div>
+            <div>
+              <h3 className="text-white text-2xl leading-relaxed mb-3">
+                Experience
+              </h3>
+            </div>
+            {aboutContent.experiences.map((experience) => (
+              <div key={experience.name} className="mb-6">
+                <div className="text-green">
+                  <p className="text-base sm:text-lg leading-relaxed font-semibold">
+                    {experience.name}
+                  </p>
+                  <p className="text-base sm:text-lg leading-relaxed font-semibold">
+                    {experience.date}
+                  </p>
+                </div>
+
+                <ul className="list-disc list-outside mt-2 pl-6 space-y-2 text-gray-400">
+                  {experience.desc.map((d, i) => (
+                    <li key={i}>
+                      <span className=" text-base sm:text-md leading-relaxed">
+                        {d.content}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
 
           <p className="text-gray-400 text-base text-md sm:text-lg leading-relaxed">
--- src/configs/about/aboutContent.ts
+++ src/configs/about/aboutContent.ts
@@ -3,55 +3,58 @@ export interface Skill {
   icon: string;
 }
 
+export interface Experience {
+  name: string;
+  date: string;
+  desc: Desc[];
+}
+
+export interface Desc {
+  content: string;
+}
+
 export interface AboutContent {
   description: string;
   skills: Skill[];
+  experiences: Experience[];
   goals: string;
 }
 
 export const aboutContent: AboutContent = {
   description: `
-    Saya seorang Frontend Developer yang berfokus pada membangun web modern
-    menggunakan React, Next.js, dan Tailwind CSS. 
-    Memiliki pengalaman membuat UI yang responsif, animasi halus, dan integrasi API.
+    Saya adalah seorang Frontend Developer dengan minat besar pada pengembangan
+    antarmuka web modern. Saya sering menggunakan Next.js (dengan TypeScript/TSX)
+    dan TailwindCSS sebagai stack utama untuk membangun website yang responsif,
+    cepat, dan mudah dikembangkan. Saya juga menempuh pendidikan di SMKN 1 Kediri, 
+    jurusan Teknik Komputer dan Jaringan (TKJ).
   `,
   skills: [
+    { name: "HTML", icon: "/assets/images/svg/tech/html.svg" },
+    { name: "CSS", icon: "/assets/images/svg/tech/css.svg" },
+    { name: "JavaScript", icon: "/assets/images/svg/tech/js.svg" },
+    { name: "React", icon: "/assets/images/svg/tech/react.svg" },
+    { name: "TypeScript", icon: "/assets/images/svg/tech/ts.svg" },
+    { name: "TailwindCSS", icon: "/assets/images/svg/tech/tailwind.svg" },
+    { name: "SASS", icon: "/assets/images/svg/tech/sass.svg" },
+    { name: "Next.js", icon: "/assets/images/svg/tech/next.svg" },
+  ],
+  experiences: [
     {
-      name: "HTML",
-      icon: "/assets/images/svg/tech/html.svg",
-    },
-    {
-      name: "CSS",
-      icon: "/assets/images/svg/tech/css.svg",
-    },
-    {
-      name: "JavaScript",
-      icon: "/assets/images/svg/tech/js.svg",
-    },
-    {
-      name: "Next.js",
-      icon: "/assets/images/svg/tech/next.svg",
-    },
-    {
-      name: "React",
-      icon: "/assets/images/svg/tech/react.svg",
-    },
-    {
-      name: "TypeScript",
-      icon: "/assets/images/svg/tech/ts.svg",
-    },
-    {
-      name: "TailwindCSS",
-      icon: "/assets/images/svg/tech/tailwind.svg",
-    },
-    {
-      name: "SASS",
-      icon: "/assets/images/svg/tech/sass.svg",
+      name: "ORDO APPS",
+      date: "Jul 2024 - Nov 2024",
+      desc: [
+        {
+          content: `Mengonversi desain Figma menjadi halaman web yang bersih dan responsif menggunakan Laravel Blade dan SASS (SCSS) untuk landing page serta halaman dashboard admin.`,
+        },
+        {
+          content: `Memperbaiki bug frontend berdasarkan feedback QA dan berkolaborasi dengan desainer serta developer backend untuk menyempurnakan UI/UX hingga siap produksi.`,
+        },
+      ],
     },
   ],
   goals: `
-    Fokus saya adalah terus berkembang di dunia frontend, memperdalam animasi
-    dengan Framer Motion, dan menambah pengalaman di sisi backend agar bisa 
-    menjadi Fullstack Developer.
+    Saya ingin terus mengasah kemampuan di frontend development,
+    memperdalam animasi menggunakan Framer Motion, serta menambah wawasan di sisi
+    backend agar dapat berkembang menjadi seorang Fullstack Developer.
   `,
 };
